6726adaf169bd284d00a70ef539bec510e07bd3f
Jesper Stefansson <jesper.stefansson@gmail.com>
2016-11-27 11:12:28 +0000

    Make the build.bat copy across the data folder, this needs to be removed with the engine reading an environment variable for the data directory.
    Also remove the concept of debug and release build directories. At this
    point I don't have a need to keep two builds up to date and it
    simplifies the script anyway

--- build.bat
+++ build.bat
@@ -14,21 +14,17 @@ SET OPTIMIZED=/O2
 SET UNOPTIMIZED=/Od
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR%
-if not exist %BUILD_DIR%\tools mkdir %BUILD_DIR%\tools
-if not exist %BUILD_DIR%\debug mkdir %BUILD_DIR%\debug
-if not exist %BUILD_DIR%\release mkdir %BUILD_DIR%\release
-
-PUSHD %BUILD_DIR%\tools
-cl %FLAGS% %OPTIMIZED% %ROOT%\tools\preprocessor.cpp /link /SUBSYSTEM:CONSOLE
-REM cl %FLAGS% %UNOPTIMIZED% %ROOT%\tools\preprocessor.cpp /link /SUBSYSTEM:CONSOLE
-POPD
 
 SET INCLUDE_DIR=%INCLUDE_DIR% -I%VULKAN_LOCATION%\Include
 
 SET LIBS=User32.lib Shlwapi.lib Shell32.lib
 SET LIBS=%LIBS% %VULKAN_LOCATION%\Bin\vulkan-1.lib
 
-PUSHD %BUILD_DIR%\%1
-REM cl %FLAGS% &OPTIMIZED% %INCLUDE_DIR% %ROOT%\src\platform/win32_main.cpp /link %LIBS% /SUBSYSTEM:WINDOWS
-cl %FLAGS% %UNOPTIMIZED% %INCLUDE_DIR% %ROOT%\src\platform/win32_main.cpp /link %LIBS% /SUBSYSTEM:WINDOWS
-POPD 
+PUSHD %BUILD_DIR%
+cl %FLAGS% %OPTIMIZED% %ROOT%\tools\preprocessor.cpp /link /SUBSYSTEM:CONSOLE
+cl %FLAGS% %UNOPTIMIZED% %INCLUDE_DIR% /Feleary.exe %ROOT%\src\platform/win32_main.cpp /link %LIBS% /SUBSYSTEM:WINDOWS
+POPD
+
+REM TODO(jesper): remove this step and make the engine read environment
+REM variables for the data directory
+xcopy %ROOT%\data %BUILD_DIR%\data /e /i /h
