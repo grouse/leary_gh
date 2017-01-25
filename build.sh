dc231d14a4f713d612270ebf1c050bad3e293e11
Jesper Stefansson <jesper.stefansson@gmail.com>
2017-01-25 21:08:27 +0000

    Fixup the build script to use a C++ compiler with -std=c++11 option

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-FLAGS=-std=c++11 -g
-INCLUDE_DIR=-I$ROOT/src
+FLAGS="-std=c++11 -g"
+INCLUDE_DIR="-I$ROOT/src"
 
 OPTIMIZED=-O3
 UNOPTIMIZED=-O0
@@ -11,5 +11,5 @@ UNOPTIMIZED=-O0
 mkdir -p build/data/shaders
 
 pushd $ROOT/build
-$CC $FLAGS $UNOPTIMIZED $INCLUDE_DIR -o leary $ROOT/src/platform/linux_main.cpp
+$CXX $FLAGS $UNOPTIMIZED $INCLUDE_DIR -o leary $ROOT/src/platform/linux_main.cpp
 popd
