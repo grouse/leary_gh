86fac9f67b98c2ccdf86a4cddd616531ff938e82
Jesper Stefansson <jesper.stefansson@gmail.com>
2017-02-20 19:22:54 +0000

    print out build times, cause interesting

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+total_start=`date +%s%N`
+
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p build/data/shaders
@@ -21,6 +23,7 @@ TOOLS_FLAGS="$FLAGS $WARNINGS $UNOPTIMIZED $INCLUDE_DIR"
 
 
 pushd $ROOT/build
+assets_start=`date +%s%N`
 glslangValidator -V $ROOT/src/render/shaders/generic.vert -o $ROOT/build/data/shaders/generic.vert.spv
 glslangValidator -V $ROOT/src/render/shaders/generic.frag -o $ROOT/build/data/shaders/generic.frag.spv
 
@@ -28,7 +31,27 @@ glslangValidator -V $ROOT/src/render/shaders/font.vert -o $ROOT/build/data/shade
 glslangValidator -V $ROOT/src/render/shaders/font.frag -o $ROOT/build/data/shaders/font.frag.spv
 
 cp -R $ROOT/assets/fonts $ROOT/build/data/fonts
+assets_end=`date +%s%N`
 
+leary_start=`date +%s%N`
 $CXX $LEARY_FLAGS -o leary $ROOT/src/platform/linux_main.cpp
+leary_end=`date +%s%N`
+
+tools_start=`date +%s%N`
 $CXX $TOOLS_FLAGS -o preprocessor $ROOT/tools/preprocessor.cpp
+tools_end=`date +%s%N`
 popd
+
+total_end=`date +%s%N`
+
+assets_duration=$(((assets_end-assets_start)/1000000))
+leary_duration=$(((leary_end-leary_start)/1000000))
+tools_duration=$(((tools_end-tools_start)/1000000))
+total_duration=$(((total_end-total_start)/1000000))
+
+echo "assets built in: $assets_duration ms"
+echo "leary built in: $leary_duration ms"
+echo "tools built in: $tools_duration ms"
+echo "total: $total_duration ms"
+
+
