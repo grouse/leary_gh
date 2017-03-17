634ec1ae977c1506778301df161afd8803d025fa
Jesper Stefansson <jesper.stefansson@gmail.com>
2017-03-17 21:44:14 +0000

    Big cleanup on allocator benchmark to printouts and method etc

--- tools/benchmark.cpp
+++ tools/benchmark.cpp
@@ -27,27 +27,22 @@ i64 get_time_difference(timespec start, timespec end)
 	return difference;
 }
 
-i64 benchmark_stack_allocator()
+#define NUM_16B  (50000)
+#define NUM_256B (5000)
+#define NUM_8K   (500)
+#define NUM_2MB  (50)
+
+i64 benchmark_stack(isize alloc_size, i32 count)
 {
-	isize size = 256 * 1024 * 1024;
+	isize size = alloc_size * 2;
 	void *buffer = malloc(size);
 
 	StackAllocator stack = make_stack_allocator(buffer, size);
 
 	timespec start = get_time();
 
-	for (i32 i = 0; i < 10000; i++) {
-		void *tmp = alloc(&stack, 16);
-		dealloc(&stack, tmp);
-	}
-
-	for (i32 i = 0; i < 1000; i++) {
-		void *tmp = alloc(&stack, 256);
-		dealloc(&stack, tmp);
-	}
-
-	for (i32 i = 0; i < 50; i++) {
-		void *tmp = alloc(&stack, 2 * 1024 * 1024);
+	for (i32 i = 0; i < count; i++) {
+		void *tmp = alloc(&stack, alloc_size);
 		dealloc(&stack, tmp);
 	}
 
@@ -57,33 +52,20 @@ i64 benchmark_stack_allocator()
 	return get_time_difference(start, end);
 }
 
-i64 benchmark_linear_allocator()
+i64 benchmark_linear(isize alloc_size, i32 count)
 {
-	isize size = 256 * 1024 * 1024;
+	isize size = alloc_size * count * 2;
 	void *buffer = malloc(size);
 
 	LinearAllocator a = make_linear_allocator(buffer, size);
 
 	timespec start = get_time();
 
-	for (i32 i = 0; i < 10000; i++) {
-		void *tmp = alloc(&a, 16);
+	for (i32 i = 0; i < count; i++) {
+		void *tmp = alloc(&a, alloc_size);
 		VAR_UNUSED(tmp);
 	}
 
-	reset(&a);
-
-	for (i32 i = 0; i < 1000; i++) {
-		void *tmp = alloc(&a, 256);
-		VAR_UNUSED(tmp);
-	}
-
-	reset(&a);
-
-	for (i32 i = 0; i < 50; i++) {
-		void *tmp = alloc(&a, 2 * 1024 * 1024);
-		VAR_UNUSED(tmp);
-	}
 	reset(&a);
 
 	timespec end = get_time();
@@ -92,22 +74,12 @@ i64 benchmark_linear_allocator()
 	return get_time_difference(start, end);
 }
 
-i64 benchmark_malloc()
+i64 benchmark_malloc(isize alloc_size, i32 count)
 {
 	timespec start = get_time();
 
-	for (i32 i = 0; i < 10000; i++) {
-		void *tmp = malloc(16);
-		free(tmp);
-	}
-
-	for (i32 i = 0; i < 1000; i++) {
-		void *tmp = malloc(256);
-		free(tmp);
-	}
-
-	for (i32 i = 0; i < 50; i++) {
-		void *tmp = malloc(2 * 1024 * 1024);
+	for (i32 i = 0; i < count; i++) {
+		void *tmp = malloc(alloc_size);
 		free(tmp);
 	}
 
@@ -115,30 +87,117 @@ i64 benchmark_malloc()
 	return get_time_difference(start, end);
 }
 
-#define MIN(a, b) (a) < (b) ? (a) : (b)
-#define MAX(a, b) (a) > (b) ? (a) : (b)
-
 int main(int, char**)
 {
-	i64 malloc_average = 0;
-	i64 linear_average = 0;
-	i64 stack_average  = 0;
+	i64 malloc_average_16B  = 0;
+	i64 malloc_average_256B = 0;
+	i64 malloc_average_8K   = 0;
+	i64 malloc_average_2MB  = 0;
+
+	i64 linear_average_16B  = 0;
+	i64 linear_average_256B = 0;
+	i64 linear_average_8K   = 0;
+	i64 linear_average_2MB  = 0;
+
+	i64 stack_average_16B  = 0;
+	i64 stack_average_256B = 0;
+	i64 stack_average_8K   = 0;
+	i64 stack_average_2MB  = 0;
 
 	// NOTE(jesper): we run each benchmark a thousand times and use the average
 	// for our final result to rule out outliers from the system scheduler and
 	// caching
 	for (i32 i = 0; i < 1000; i++) {
-		i64 malloc_duration = benchmark_malloc();
-		i64 linear_duration = benchmark_linear_allocator();
-		i64 stack_duration  = benchmark_stack_allocator();
+		i64 malloc_duration_16B  = benchmark_malloc(16, NUM_16B);
+		i64 malloc_duration_256B = benchmark_malloc(256, NUM_256B);
+		i64 malloc_duration_8K   = benchmark_malloc(256, NUM_8K);
+		i64 malloc_duration_2MB  = benchmark_malloc(2 * 1024 * 1024, NUM_2MB);
 
-		malloc_average = (malloc_average + malloc_duration) / 2;
-		linear_average = (linear_average + linear_duration) / 2;
-		stack_average = (stack_average + stack_duration) / 2;
+		i64 linear_duration_16B  = benchmark_linear(16, NUM_16B);
+		i64 linear_duration_256B = benchmark_linear(256, NUM_256B);
+		i64 linear_duration_8K   = benchmark_linear(256, NUM_8K);
+		i64 linear_duration_2MB  = benchmark_linear(2 * 1024 * 1024, NUM_2MB);
+
+		i64 stack_duration_16B  = benchmark_stack(16, NUM_16B);
+		i64 stack_duration_256B = benchmark_stack(256, NUM_256B);
+		i64 stack_duration_8K   = benchmark_stack(8 * 1024, NUM_8K);
+		i64 stack_duration_2MB  = benchmark_stack(2 * 1024 * 1024, NUM_2MB);
+
+
+		malloc_average_16B  = (malloc_average_16B + malloc_duration_16B) / 2;
+		malloc_average_256B = (malloc_average_256B + malloc_duration_256B) / 2;
+		malloc_average_8K   = (malloc_average_8K + malloc_duration_8K) / 2;
+		malloc_average_2MB  = (malloc_average_2MB + malloc_duration_2MB) / 2;
+
+		linear_average_16B  = (linear_average_16B + linear_duration_16B) / 2;
+		linear_average_256B = (linear_average_256B + linear_duration_256B) / 2;
+		linear_average_8K   = (linear_average_8K + linear_duration_8K) / 2;
+		linear_average_2MB  = (linear_average_2MB + linear_duration_2MB) / 2;
+
+		stack_average_16B   = (stack_average_16B + stack_duration_16B) / 2;
+		stack_average_256B  = (stack_average_256B + stack_duration_256B) / 2;
+		stack_average_8K    = (stack_average_8K + stack_duration_8K) / 2;
+		stack_average_2MB   = (stack_average_2MB + stack_duration_2MB) / 2;
 	}
 
-	DEBUG_LOG("malloc finished in %f ms", (f32)malloc_average / 1000000.0f);
-	DEBUG_LOG("linear finished in %f ms", (f32)linear_average / 1000000.0f);
-	DEBUG_LOG("stack  finished in %f ms", (f32)stack_average / 1000000.0f);
+	f32 malloc_average_16B_um  = (f32)malloc_average_16B / 1000.0f;
+	f32 malloc_average_256B_um = (f32)malloc_average_256B / 1000.0f;
+	f32 malloc_average_8K_um   = (f32)malloc_average_8K / 1000.0f;
+	f32 malloc_average_2MB_um  = (f32)malloc_average_2MB / 1000.0f;
+
+	f32 linear_average_16B_um  = (f32)linear_average_16B / 1000.0f;
+	f32 linear_average_256B_um = (f32)linear_average_256B / 1000.0f;
+	f32 linear_average_8K_um   = (f32)linear_average_8K / 1000.0f;
+	f32 linear_average_2MB_um  = (f32)linear_average_2MB / 1000.0f;
+
+	f32 stack_average_16B_um  = (f32)stack_average_16B / 1000.0f;
+	f32 stack_average_256B_um = (f32)stack_average_256B / 1000.0f;
+	f32 stack_average_8K_um   = (f32)stack_average_8K / 1000.0f;
+	f32 stack_average_2MB_um  = (f32)stack_average_2MB / 1000.0f;
+
+	DEBUG_LOG("system malloc:");
+	DEBUG_LOG("- 16B  [%d]:\t%f um \t(%f per alloc)", NUM_16B,
+	          malloc_average_16B_um, malloc_average_16B_um / NUM_16B);
+
+	DEBUG_LOG("- 256B [%d]: \t%f um \t(%f per alloc)", NUM_256B,
+	          malloc_average_256B_um, malloc_average_256B_um / NUM_256B);
+
+	DEBUG_LOG("- 8K   [%d]: \t%f um \t(%f per alloc)", NUM_8K,
+	          malloc_average_8K_um, malloc_average_8K_um / NUM_8K);
+
+	DEBUG_LOG("- 2MB  [%d]: \t%f um \t(%f per alloc)", NUM_2MB,
+	          malloc_average_2MB_um, malloc_average_2MB_um / NUM_2MB);
+
+	DEBUG_LOG("");
+
+	DEBUG_LOG("custom linear:");
+	DEBUG_LOG("- 16B  [%d]:\t%f um \t(%f per alloc)", NUM_16B,
+	          linear_average_16B_um, linear_average_16B_um / NUM_16B);
+
+	DEBUG_LOG("- 256B [%d]: \t%f um \t(%f per alloc)", NUM_256B,
+	          linear_average_256B_um, linear_average_256B_um / NUM_256B);
+
+	DEBUG_LOG("- 8K   [%d]: \t%f um \t(%f per alloc)", NUM_8K,
+	          linear_average_8K_um, linear_average_8K_um / NUM_8K);
+
+	DEBUG_LOG("- 2MB  [%d]: \t%f um \t(%f per alloc)", NUM_2MB,
+	          linear_average_2MB_um, linear_average_2MB_um / NUM_2MB);
+
+	DEBUG_LOG("");
+
+	DEBUG_LOG("custom stack:");
+	DEBUG_LOG("- 16B  [%d]:\t%f um \t(%f per alloc)", NUM_16B,
+	          stack_average_16B_um, stack_average_16B_um / NUM_16B);
+
+	DEBUG_LOG("- 256B [%d]: \t%f um \t(%f per alloc)", NUM_256B,
+	          stack_average_256B_um, stack_average_256B_um / NUM_256B);
+
+	DEBUG_LOG("- 8K   [%d]: \t%f um \t(%f per alloc)", NUM_8K,
+	          stack_average_8K_um, stack_average_8K_um / NUM_8K);
+
+	DEBUG_LOG("- 2MB  [%d]: \t%f um \t(%f per alloc)", NUM_2MB,
+	          stack_average_2MB_um, stack_average_2MB_um / NUM_2MB);
 	return 0;
 }
+
+
