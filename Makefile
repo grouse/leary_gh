7a04de4ac17429768e8e3151d1050df0b448a3a5
Jesper Stefansson <jesper.stefansson@gmail.com>
2017-03-25 20:53:17 +0000

    Add Makefile to build on Linux to speed up build times by about 50%
    This is true for both full rebuilds and partial builds right now because the
    bottleneck is the code compilation, for which I'm not using any dependency
    generation right now.
    
    One second rebuilds is fast enough for now :)

---
+++ Makefile
@@ -0,0 +1,81 @@
+
+ROOT  = .
+BUILD = ./build
+
+.DEFAULT_GOAL := all
+
+SPV_SRC = $(ROOT)/src/render/shaders
+SPV_DST = $(BUILD)/data/shaders
+SPV_VERT = $(addprefix $(SPV_DST)/,$(addsuffix .vert.spv,generic font mesh terrain))
+SPV_FRAG = $(addprefix $(SPV_DST)/,$(addsuffix .frag.spv,generic font mesh terrain))
+
+$(SPV_DST)/%.vert.spv: $(SPV_SRC)/%.vert
+	glslangValidator -V $< -o $@
+
+$(SPV_DST)/%.frag.spv: $(SPV_SRC)/%.frag
+	glslangValidator -V $< -o $@
+
+shaders: | $(SPV_DST) $(SPV_VERT) $(SPV_FRAG)
+
+
+FONT_SRC = $(ROOT)/assets/fonts
+FONT_DST = $(BUILD)/data/fonts
+FONTS = $(addprefix $(FONT_DST)/,Roboto-Regular.ttf)
+
+$(FONT_DST)/%.ttf: $(FONT_SRC)/%.ttf
+	cp $< $@
+
+fonts: | $(FONT_DST) $(FONTS)
+
+
+MODELS_SRC = $(ROOT)/assets/models
+MODELS_DST = $(BUILD)/data/models
+MODELS = $(addprefix $(MODELS_DST)/,cube.obj armoire.obj)
+
+$(MODELS_DST)/%.obj: $(MODELS_SRC)/%.obj
+	cp $< $@
+
+models: | $(MODELS_DST) $(MODELS)
+
+
+FLAGS = -std=c++11 -g
+
+NOWARNINGS = -Wno-int-to-void-pointer-cast -Wno-nested-anon-types -Wno-unused-function
+WARNINGS   = -Wall -Wextra -Wpedantic $(NOWARNINGS)
+
+INCLUDE_DIR = -I$(ROOT)/src -I$(ROOT)
+
+OPTIMIZED   = -O3
+UNOPTIMIZED = -O0
+
+LEARY_LIBS  = -lvulkan -lX11 -lXi -ldl
+LEARY_FLAGS = $(FLAGS) $(WARNINGS) $(UNOPTIMIZED) $(INCLUDE_DIR)
+
+$(BUILD)/game.so: FORCE
+	$(CXX) $(LEARY_FLAGS) -fPIC -shared $(ROOT)/src/platform/linux_leary.cpp -o $@
+
+$(BUILD)/leary: FORCE
+	$(CXX) $(LEARY_FLAGS) $(LEARY_LIBS) -o $@ $(ROOT)/src/platform/linux_main.cpp
+
+leary: $(BUILD)/game.so $(BUILD)/leary
+
+
+TOOLS_FLAGS = $(FLAGS) $(WARNINGS) $(UNOPTIMIZED) $(INCLUDE_DIR)
+$(BUILD)/preprocessor: FORCE
+	$(CXX) $(TOOLS_FLAGS) $(ROOT)/tools/preprocessor.cpp -o $@
+
+tools: $(BUILD)/preprocessor
+
+
+all: shaders fonts models leary tools
+
+$(SPV_DST):
+	mkdir -p $(SPV_DST)
+
+$(FONT_DST):
+	mkdir -p $(FONT_DST)
+
+$(MODELS_DST):
+	mkdir -p $(MODELS_DST)
+
+FORCE:
--- build.sh
+++ build.sh
@@ -1,66 +1,7 @@
 #!/bin/bash
 
-total_start=`date +%s%N`
-
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p build/data/shaders
-
-
-FLAGS="-std=c++11 -g"
-
-NOWARNINGS="-Wno-int-to-void-pointer-cast -Wno-nested-anon-types -Wno-unused-function"
-WARNINGS="-Wall -Wextra -Wpedantic $NOWARNINGS"
-
-INCLUDE_DIR="-I$ROOT/src -I$ROOT"
-
-OPTIMIZED=-O3
-UNOPTIMIZED=-O0
-
-LEARY_LIBS="-lvulkan -lX11 -lXi -ldl"
-LEARY_FLAGS="$FLAGS $WARNINGS $UNOPTIMIZED $INCLUDE_DIR"
-TOOLS_FLAGS="$FLAGS $WARNINGS $UNOPTIMIZED $INCLUDE_DIR"
-
-
-pushd $ROOT/build
-assets_start=`date +%s%N`
-glslangValidator -V $ROOT/src/render/shaders/generic.vert -o $ROOT/build/data/shaders/generic.vert.spv
-glslangValidator -V $ROOT/src/render/shaders/generic.frag -o $ROOT/build/data/shaders/generic.frag.spv
-
-glslangValidator -V $ROOT/src/render/shaders/font.vert -o $ROOT/build/data/shaders/font.vert.spv
-glslangValidator -V $ROOT/src/render/shaders/font.frag -o $ROOT/build/data/shaders/font.frag.spv
-
-glslangValidator -V $ROOT/src/render/shaders/mesh.vert -o $ROOT/build/data/shaders/mesh.vert.spv
-glslangValidator -V $ROOT/src/render/shaders/mesh.frag -o $ROOT/build/data/shaders/mesh.frag.spv
-
-glslangValidator -V $ROOT/src/render/shaders/terrain.vert -o $ROOT/build/data/shaders/terrain.vert.spv
-glslangValidator -V $ROOT/src/render/shaders/terrain.frag -o $ROOT/build/data/shaders/terrain.frag.spv
-
-cp -R $ROOT/assets/fonts $ROOT/build/data/fonts
-cp -R $ROOT/assets/models $ROOT/build/data/models
-assets_end=`date +%s%N`
-
-leary_start=`date +%s%N`
-$CXX $LEARY_FLAGS -fPIC -shared $ROOT/src/platform/linux_leary.cpp -o game.so
-$CXX $LEARY_FLAGS $LEARY_LIBS -o leary $ROOT/src/platform/linux_main.cpp
-leary_end=`date +%s%N`
-
-tools_start=`date +%s%N`
-$CXX $TOOLS_FLAGS -o preprocessor $ROOT/tools/preprocessor.cpp
-#$CXX $TOOLS_FLAGS -O0 -o benchmark    $ROOT/tools/benchmark.cpp
-tools_end=`date +%s%N`
+pushd $ROOT
+make -Oline -j12
 popd
-
-total_end=`date +%s%N`
-
-assets_duration=$(((assets_end-assets_start)/1000000))
-leary_duration=$(((leary_end-leary_start)/1000000))
-tools_duration=$(((tools_end-tools_start)/1000000))
-total_duration=$(((total_end-total_start)/1000000))
-
-echo "assets built in: $assets_duration ms"
-echo "leary built in: $leary_duration ms"
-echo "tools built in: $tools_duration ms"
-echo "total: $total_duration ms"
-
-
